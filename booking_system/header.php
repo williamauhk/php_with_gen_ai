34b2738cdb2852bfebfae04436b821c6d84e35b7
W <iblogger@gmail.com>
2024-01-08 19:08:14 +0800

    change title

--- booking_system/header.php
+++ booking_system/header.php
@@ -7,7 +7,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Menu</title><script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <title>ABC Exchange Company</title><script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
